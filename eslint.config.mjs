a0642a39a17663ee802157524fcf4a69dc51d1f1
Your Name <hiteshsachdeva343@gmail.com>
2025-09-09 16:24:55 -0400

    upadated eslint.conf for dependency

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,8 @@
-import globals from 'globals';
-import pluginJs from '@eslint/js';
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
 
-export default [
-  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
-  { languageOptions: { globals: globals.node } },
-  pluginJs.configs.recommended,
-];
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+]);

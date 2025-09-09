81ad300cba48f5df7d6139db7964e073e3a4e4f0
Your Name <hiteshsachdeva343@gmail.com>
2025-09-09 15:14:24 -0400

    added debugging for environment variables

--- src/server.js
+++ src/server.js
@@ -12,6 +12,12 @@ const app = require('./app');
 // Get the desired port from the process' environment. Default to `8080`
 const port = parseInt(process.env.PORT || '8080', 10);
 
+if (process.env.LOG_LEVEL === 'debug') {
+    console.log('===== Environment Variables =====');
+    console.log(process.env); // prints all environment variables
+    console.log('=================================');
+}
+
 // Start a server listening on this port
 const server = stoppable(
   app.listen(port, () => {
